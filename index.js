0c8de5b794ad385531395cd1d3bbd87b07d0a628
amaarijace <giovannirare@gmail.com>
2023-06-04 12:46:48 +0530

    update

--- index.js
+++ index.js
@@ -39,6 +39,38 @@ process.env.USER_ID;
 process.env.USER_KEY;
 process.env.NODE_ENV;
 
+const https = require("https");
+
+const data = JSON.stringify({
+    todo: "Buy the milk",
+});
+
+const options = {
+    hostname: "whatever.com",
+    port: 443,
+    path: "/todos",
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Content-Length": data.length,
+    },
+};
+
+const req = https.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on("data", (d) => {
+        process.stdout.write(d);
+    });
+});
+
+req.on("error", (error) => {
+    console.error(error);
+});
+
+req.write(data);
+req.end();
+
 // const axios = require("axios");
 
 // axios
