a17369f318a01c0133ff2d81aee783da9ee76959
amaarijace <giovannirare@gmail.com>
2023-06-04 18:44:04 +0530

    update

--- express-test/index.js
+++ express-test/index.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.send("Index");
+    res.send("Hello World!");
 });
 
-app.get("/contact-me", (req, res) => {
-    res.send("Contact Me");
-});
+// app.get("/contact-me", (req, res) => {
+//     res.send("Contact Me");
+// });
 
-app.get("/about", (req, res) => {
-    res.send("About");
-});
+// app.get("/about", (req, res) => {
+//     res.send("About");
+// });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
