0f3688cd26b08e3b88b340e6ce121938b1c8083a
AP2K3 <arish2k3@gmail.com>
2023-06-10 17:35:31 +0530

    update

--- express-test/index.js
+++ express-test/index.js
@@ -5,5 +5,5 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
