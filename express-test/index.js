e08df8df6b0f446887b6363e7ff0ee0a2e03faa9
AP2K3 <arish2k3@gmail.com>
2023-06-13 09:50:23 +0530

    update

--- express-test/index.js
+++ express-test/index.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
-app.use((req, res, next) => {
-  const options = {
-    root: path.join(__dirname),
-  };
+// app.use((req, res, next) => {
+//   const options = {
+//     root: path.join(__dirname),
+//   };
 
-  const fileName = "public/404/index.html";
-});
+//   const fileName = "public/404/index.html";
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
