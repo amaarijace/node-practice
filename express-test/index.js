6bab4f165f7c7f4796c01dd0788df1177bc97818
ap2k3 <arish2k3@gmail.com>
2023-06-15 15:21:25 +0530

    update

--- express-test/index.js
+++ express-test/index.js
@@ -6,7 +6,13 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-    res.status(404).send("404 not found");
+    const options = {
+        root: path.join(__dirname, "public/404"),
+    };
+
+    const fileName = "index.html";
+
+    res.status(404).sendFile(fileName, options);
 });
 
 app.listen(port, () => {
