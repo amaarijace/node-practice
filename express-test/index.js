720c413d8957ccf38215fbb8c085c6c1403af6b1
ap2k3 <arish2k3@gmail.com>
2023-06-15 15:00:42 +0530

    update

--- express-test/index.js
+++ express-test/index.js
@@ -5,6 +5,10 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+    res.status(404).send("404 not found");
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
