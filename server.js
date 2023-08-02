c7c2cabc1f7d9135d676b9a012250035710da5ea
fibbbonachos <arish2k3@gmail.com>
2023-08-02 10:47:01 +0530

    update

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  res.setHeader("Content-Type", "text/html");
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text'><button>Send</button></form></body>"
+    );
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(3000);
