8e187a03d7f7b75ebc4f6e274bad31e1e263cfe1
fibbbonachos <arish2k3@gmail.com>
2023-08-02 10:51:33 +0530

    update

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ const server = http.createServer((req, res) => {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
-      "<body><form action='/message' method='POST'><input type='text'><button>Send</button></form></body>"
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button>Send</button></form></body>"
     );
     res.write("</html>");
-    res.end();
+    return res.end();
   }
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>Hello from my Node server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
