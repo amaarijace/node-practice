45df47fdda851093eea56b86fcaacd0c794dfa56
fibbbonachos <arish2k3@gmail.com>
2023-08-02 10:58:25 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
@@ -10,6 +12,10 @@ const server = http.createServer((req, res) => {
     );
     res.write("</html>");
     return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.writeHead(302, { Location: "/" });
   }
   res.write("<html>");
   res.write("<head><title>My first page</title></head>");
