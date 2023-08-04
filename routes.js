680caf8e1d435fe4aa430d5dbbf26a37fd926591
fibbbonachos <arish2k3@gmail.com>
2023-08-04 12:34:56 +0530

    update

--- routes.js
+++ routes.js
@@ -1,5 +1,36 @@
-const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req, res) => {});
+const requestHandler = (res, req) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message, (err) => {
+        res.writeHead(404, { Location: "/" });
+        return res.end();
+      });
+    });
+  }
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>Hello from my Node server!</h1></body>");
+  res.write("</html>");
+  res.end();
+};
 
-server.listen(3000);
+module.exports = requestHandler;
