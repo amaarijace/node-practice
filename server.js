6f6bd42eb8f10cf41bfb93072298d3d6df410583
fibbbonachos <arish2k3@gmail.com>
2023-08-07 01:11:35 +0530

    update

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(adminRoutes);
 app.use(shopRoutes);
 app.use((req, res, next) => {
