bb246d2fb1a9fef4f82c44d4f7ed968c83203427
fibbbonachos <arish2k3@gmail.com>
2023-08-07 11:50:33 +0530

    update

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const adminRouter = express.Router();
 
+// /admin/add-product => GET
 adminRouter.get("/add-product", (req, res, next) => {
   res.send(
     "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Submit</button></form>"
   );
 });
 
+// /admin/add-product => POST
 adminRouter.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
